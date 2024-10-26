eccb5a3c3d639a8a797ac9ced771417eb70700d1
Elenskiy Eduard Vladislavovich <eelenskiy@ozon.ru>
2024-10-26 15:15:32 +0300

    Initial Commit

---
+++ FileX/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FileX
+//
+//  Created by eelenskiy on 26.10.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
